a40e7a803abd5354dce6e0eedbfa85b8eb702205
divagueame <martin.arce.teixeira@gmail.com>
2024-09-28 12:02:53 +0200

    Improve script

--- lua/lacasitos/get_user_choice.lua
+++ lua/lacasitos/get_user_choice.lua
@@ -1,54 +1,67 @@
-vim.api.nvim_command('highlight AAABoomFloatingWindowBorder guifg=#ff0000 guibg=#ffffff') -- Example: Red border on white background
-vim.api.nvim_command('highlight AAABoomFloatingWindowContent guifg=#000000 guibg=NONE') -- Black text on white background
+vim.api.nvim_command('highlight LacasitosFloatingWindowBorder guifg=#3f3f0f guibg=#cdcdcd') 
+vim.api.nvim_command('highlight LacasitosFloatingWindowContent guifg=#cdcdcd guibg=NONE')
+vim.api.nvim_command('highlight LacasitosFloatingWindowPrefix guifg=#F38795 guibg=NONE')
 
 local function create_floating_window(content)
   local buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, content)
 
   for i = 0, #content - 1 do
-    vim.api.nvim_buf_add_highlight(buf, -1, "AAABoomFloatingWindowContent", i, 0, -1)
+    vim.api.nvim_buf_add_highlight(buf, -1, "LacasitosFloatingWindowPrefix", i, 1, 2)
+    vim.api.nvim_buf_add_highlight(buf, -1, "LacasitosFloatingWindowContent", i, 2, -1)
   end
 
-  local width = 40
-  local height = #content + 2
+  local max_width = 0
+  for _, line in ipairs(content) do
+    max_width = math.max(max_width, #line)
+  end
+
+  local padding = 2
+  local width = max_width + padding
+  local height = #content
+
   local opts = {
-    relative = 'editor',
     width = width,
     height = height,
+    style = 'minimal',
+    relative = 'editor',
     col = (vim.o.columns - width) / 2,
     row = (vim.o.lines - height) / 2,
-    style = 'minimal',
-    border = 'rounded',
+    border = "rounded",
+    title = { {" Choose ", "Label"} },
+    title_pos = 'left',
   }
 
-  local win_id = vim.api.nvim_open_win(buf, true, opts)
-  vim.api.nvim_win_set_option(win_id, 'winhl', 'Normal:AAABoomFloatingWindowContent,FloatBorder:AAABoomFloatingWindowBorder')
-
-  return win_id
+  return vim.api.nvim_open_win(buf, true, opts)
 end
 
-local get_user_choice = function(args, callback)
 
+local get_user_choice = function(args)
+  local custom_chars = {
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+    '-', '_', '=', '+', '[', ']', '{', '}', ';', ':',
+    '\'', '"', ',', '.', '<', '>', '/', '?', '\\'
+  }
   local options = {}
   local display_content = {}
   for i, file in ipairs(args) do
-    local letter = string.char(96 + i)
+    local letter = custom_chars[i]
     options[letter] = file
-    table.insert(display_content, string.format("%s: %s", letter, file))
+    table.insert(display_content, string.format(" %s %s", letter, file))
   end
 
   local win_id = create_floating_window(display_content)
+  vim.cmd("redraw")
 
-  vim.defer_fn(function()
-    local char = vim.fn.getchar()
-    local key = vim.fn.nr2char(char)
-    print("User input: " .. key)
+  local char = vim.fn.getchar()
+  local key = vim.fn.nr2char(char)
+  vim.api.nvim_win_close(win_id, true)
 
-    vim.api.nvim_win_close(win_id, true)
-
-    callback(key)
-  end, 100)
-
+  return options[key]
 end
 
 return get_user_choice
